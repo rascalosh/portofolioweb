006e2e892505dbbc64cd3dfd168bf6b1e18061cd
rascalosh <willbertlian@gmail.com>
2026-02-26 11:42:48 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import 'iconify-icon';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
